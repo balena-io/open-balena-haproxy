176199e4d16f4c6e9833002b2e8c2f4d19202a6f
Michael Angelos Simos <michalis@resin.io>
2018-09-20 19:53:16 +0300

    generate: Change default config file input path to /.balena/config.json
    Change-type: patch
    Signed-off-by: Michael Angelos Simos <michalis@resin.io>

--- generate-cfg/generate.js
+++ generate-cfg/generate.js
@@ -3,7 +3,7 @@
 const generateHaproxyConfig = require('./generate-haproxy-cfg').generateHaproxyConfig
 
 generateHaproxyConfig(
-	'/usr/src/app/proxy-config.json',
+	'/.balena/config.json',
 	'/usr/local/etc/haproxy/haproxy.cfg',
 	'/etc/ssl/private/haproxy.cert.chain.pem'
 )
